e5bf8377974b4d17d92828826f6bff19659974c2
solov9ev <a.solov9ev@yandex.ru>
2019-10-17 22:35:02 +0300

    Метод Api.V1.Auth.AuthController.ping/2

---
+++ lib/behemoth_web/controllers/api/v1/auth/auth_controller.ex
@@ -0,0 +1,17 @@
+defmodule BehemothWeb.Api.V1.Auth.AuthController do
+
+    use BehemothWeb, :controller
+    use PhoenixSwagger
+
+    import Plug.Conn.Status, only: [code: 1]
+
+    swagger_path :ping do
+        get("/api/v1/auth/ping")
+        description("Проверка валидности доступа к закрытым токеном функциям  API")
+
+        response(code(:ok), %{"message" => "pong"})
+        response(code(:unauthorized), %{"error" => "Correct auth token must be provided."})
+      end
+
+      def ping(%Plug.Conn{} = conn, _params), do: json(conn, %{"message" => "pong"})
+end
--- lib/behemoth_web/router.ex
+++ lib/behemoth_web/router.ex
@@ -5,15 +5,35 @@ defmodule BehemothWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", BehemothWeb do
+  scope "/api", BehemothWeb.Api, as: :api do
     pipe_through :api
+
+    scope "/v1", V1, as: :v1 do
+      scope "/auth", Auth, as: :auth do
+        get "/ping", AuthController, :ping
+      end
+    end
+  end
+
+  scope "/swagger" do
+    forward "/", PhoenixSwagger.Plug.SwaggerUI, otp_app: :behemoth, swagger_file: "swagger.json"
   end
 
   def swagger_info do
     %{
       info: %{
         version: "1.0",
-        title: "Behemoth"
+        title: "Behemoth API",
+        description: """
+        ## Возможности: описание скоро будет доступно
+        """
+      },
+      securityDefinitions: %{
+        api_auth: %{
+          type: "apiKey",
+          name: "Authorization",
+          in: "header"
+        }
       }
     }
   end
