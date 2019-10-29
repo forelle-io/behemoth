33186f081440dd0b0597cb368c7c8bfb20a37785
yadyginid <48806320+yadyginid@users.noreply.github.com>
2019-10-29 22:22:01 +0300

    [DEV-9] Модель Auth.SmsCode (#2)

---
+++ priv/repo/migrations/20201027184317_create_auth_sms_codes.exs.exs
@@ -0,0 +1,17 @@
+defmodule Behemoth.Repo.Migrations.CreateAuthSmsCodes do
+  @moduledoc false
+  use Ecto.Migration
+
+  def change do
+    create table(:"auth.sms_codes") do
+      add :struct_type, :string
+      add :struct_id, :integer
+      add :auth_code, :string
+      add :confirmed_at, :naive_datetime
+
+      timestamps()
+    end
+
+    create index(:"auth.sms_codes", [:struct_type, :struct_id])
+  end
+end
