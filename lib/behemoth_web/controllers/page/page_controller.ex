c583bb085082aeceaa07eea5f613d8ef65d1a823
yadyginid <48806320+yadyginid@users.noreply.github.com>
2019-11-30 20:05:11 +0300

    [BACKEND-38] Рутовый Page Controller (#14)

---
+++ lib/behemoth_web/controllers/page/page_controller.ex
@@ -0,0 +1,12 @@
+defmodule BehemothWeb.PageController do
+  @moduledoc false
+
+  use BehemothWeb, :controller
+
+  import BehemothWeb.PageView, only: [application_name: 0]
+
+  @spec index(Conn.t(), map) :: Conn.t()
+  def index(conn, _params) do
+    json(conn, %{application: application_name(), environment: Mix.env()})
+  end
+end
